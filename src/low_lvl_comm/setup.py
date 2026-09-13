9ace97d0e3fea2b73a07f5f9d1299cb7f585f3dc
RPI5_redonnukes <ganievilia0@gmail.com>
2026-09-13 07:17:21 +0300

    Action-modification for STM_comm_node

--- src/low_lvl_comm/setup.py
+++ src/low_lvl_comm/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'low_lvl_comm'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.1.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,17 +13,13 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='redonnukes',
-    maintainer_email='ganievilia0@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='Low-level communication with STM32 over USB-serial',
+    license='MIT',
     entry_points={
         'console_scripts': [
+            'STM_comm_node = low_lvl_comm.STM_comm_node:main',
         ],
     },
 )
